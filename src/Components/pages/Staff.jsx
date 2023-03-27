8796645b666b9aab5b113bb14d846cd22ffb9b2b
ericcnguyen04 <ericc.win@gmail.com>
2023-03-27 13:58:41 -0400

    reformatted

--- src/Components/pages/Staff.jsx
+++ src/Components/pages/Staff.jsx
@@ -12,22 +12,22 @@ export default function Staff() {
             </p>
 
             <div className='staff'>
-                <div>CEO Amanda</div>
-                <div>COO / COOM Rhen</div>
-                <div>CFO Rickma</div>
-                <div>CIO / CISO Frost</div>
-                <div>CTO Ryuu</div>
-                <div>CCO Matney</div>
-                <div>CKO Adiv</div>
-                <div>CDO Tea</div>
-                <div>CMO OUR FAVORITE DOG</div>
-                <div>CSO Cameron</div>
-                <div>CAO JohnIsMe</div>
-                <div>CPO NotJames</div>
-                <div>CYPO Azlanii</div>
-                <div>CHRO Treeynh</div>
-                <div>CYO Rachel</div>
-                <div>CBO Jeruki</div>
+                <div className='staffrow'><h1>CEO</h1> Amanda</div>
+                <div><h1>COO</h1> / COOM Rhen</div>
+                <div><h1>CFO</h1> Rickma</div>
+                <div><h1>CIO</h1> / CISO Frost</div>
+                <div><h1>CTO</h1> Ryuu</div>
+                <div><h1>CCO</h1> Matney</div>
+                <div><h1>CKO</h1> Adiv</div>
+                <div><h1>CDO</h1> Tea</div>
+                <div><h1>CMO</h1> OUR FAVORITE DOG</div>
+                <div><h1>CSO</h1> Cameron</div>
+                <div><h1>CAO</h1> JohnIsMe</div>
+                <div><h1>CPO</h1> NotJames</div>
+                <div><h1>CYPO</h1> Azlanii</div>
+                <div><h1>CHRO</h1> Treeynh</div>
+                <div><h1>CYO</h1> Rachel</div>
+                <div><h1>CBO</h1> Jeruki</div>
             </div>
         </div>
     )
