39d08117627669cdcadd755b571db95a89fe5b94
ericcnguyen04 <ericc.win@gmail.com>
2023-03-26 21:40:12 -0400

    inputted message

--- src/Components/pages/Staff.jsx
+++ src/Components/pages/Staff.jsx
@@ -1,9 +1,13 @@
-
+import '../../App.css'
 
 export default function Staff() {
     return (
         <div>
-            <h1>Welcome to our NEPH Matress Board of Directors</h1>
+            <h1><center>Welcome to our NEPH Matress Board of Directors</center></h1>
+            <p className='Discretion'>
+                <strong>Attention: </strong>
+                Although this sucessful corporation founded on 2023 has a chain of command, the positions and entitlement has no meaning as all authoritative figures have equal power amongst one another. Why? Because Communism!
+            </p>
         </div>
     )
 }
