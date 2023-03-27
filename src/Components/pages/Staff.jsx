be4b660b8673e5c09d8a5713ac9cda3e958a9508
ericcnguyen04 <ericc.win@gmail.com>
2023-03-26 21:45:42 -0400

    printed staff

--- src/Components/pages/Staff.jsx
+++ src/Components/pages/Staff.jsx
@@ -5,9 +5,30 @@ export default function Staff() {
         <div>
             <h1><center>Welcome to our NEPH Matress Board of Directors</center></h1>
             <p className='Discretion'>
+                <center>
                 <strong>Attention: </strong>
-                Although this sucessful corporation founded on 2023 has a chain of command, the positions and entitlement has no meaning as all authoritative figures have equal power amongst one another. Why? Because Communism!
+                Although this sucessful corporation founded on 2023 has a chain of command, the positions and entitlement has no meaning as all authoritative figures have equal power amongst one another. Why? Because we love Communism!
+                </center>
             </p>
+
+            <ul>
+            - Amanda is CEO
+            - Rhen is COO / COOM
+            - Rickma is CFO
+            - Frost is CIO / CISO
+            - Ryuu is CTO
+            - Matney is CCO
+            - Ali is CKO
+            - Tea is CDO
+            - OUR FAVORITE DOG :yogurt~1: IS CMO
+            - Cameron is CSO
+            - JohnIsMe is CAO
+            - NotJames is CPO
+            - Azlanii is CYPO
+            - Treeynh is CHRO
+            - Rachel is CYO
+            - Jeruki is CBO
+            </ul>
         </div>
     )
 }
