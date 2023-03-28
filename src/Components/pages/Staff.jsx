569b25d6cf3a8814b5a9fb8a1bb3af01613eab37
ericcnguyen04 <ericc.win@gmail.com>
2023-03-28 17:13:44 -0400

    starting

--- src/Components/pages/Staff.jsx
+++ src/Components/pages/Staff.jsx
@@ -9,25 +9,25 @@ export default function Staff() {
                 <strong>Attention: </strong>
                 Although this sucessful corporation founded on 2023 has a chain of command, the positions and entitlement has no meaning as all authoritative figures have equal power amongst one another. Why? Because we love Communism!
                 </center>
-            </p>
+            </p>cringe
 
             <div className='staff'>
                 <div className='staffrow'><h1>CEO</h1> Amanda</div>
-                <div><h1>COO</h1> / COOM Rhen</div>
-                <div><h1>CFO</h1> Rickma</div>
-                <div><h1>CIO</h1> / CISO Frost</div>
-                <div><h1>CTO</h1> Ryuu</div>
-                <div><h1>CCO</h1> Matney</div>
-                <div><h1>CKO</h1> Adiv</div>
-                <div><h1>CDO</h1> Tea</div>
-                <div><h1>CMO</h1> OUR FAVORITE DOG</div>
-                <div><h1>CSO</h1> Cameron</div>
-                <div><h1>CAO</h1> JohnIsMe</div>
-                <div><h1>CPO</h1> NotJames</div>
-                <div><h1>CYPO</h1> Azlanii</div>
-                <div><h1>CHRO</h1> Treeynh</div>
-                <div><h1>CYO</h1> Rachel</div>
-                <div><h1>CBO</h1> Jeruki</div>
+                <div className='staffrow'><h1>COO</h1> / COOM Rhen</div>
+                <div className='staffrow'><h1>CFO</h1> Rickma</div>
+                <div className='staffrow'><h1>CIO</h1> / CISO Frost</div>
+                <div className='staffrow'><h1>CTO</h1> Ryuu</div>
+                <div className='staffrow'><h1>CCO</h1> Matney</div>
+                <div className='staffrow'><h1>CKO</h1> Adiv</div>
+                <div className='staffrow'><h1>CDO</h1> Tea</div>
+                <div className='staffrow'><h1>CMO</h1> OUR FAVORITE DOG</div>
+                <div className='staffrow'><h1>CSO</h1> Cameron</div>
+                <div className='staffrow'><h1>CAO</h1> JohnIsMe</div>
+                <div className='staffrow'><h1>CPO</h1> NotJames</div>
+                <div className='staffrow'><h1>CYPO</h1> Azlanii</div>
+                <div className='staffrow'><h1>CHRO</h1> Treeynh</div>
+                <div className='staffrow'><h1>CYO</h1> Rachel</div>
+                <div className='staffrow'><h1>CBO</h1> Jeruki</div>
             </div>
         </div>
     )
